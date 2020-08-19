a138e2d9c412f34353a59bae3f4bd4cab8cb2a6b
liufuxiang101 <liufuxiang101@163.com>
2020-08-20 07:11:34 +0800

    Add Week19

---
+++ week19/19.ToyTool/generators/app/templates/animation.js
@@ -0,0 +1,130 @@
+export class Timeline {
+  constructor() {
+    this.animations = [];
+    this.requestID = null;
+    this.state = "inited";
+    this.tick = () => {
+      let t = Date.now() - this.startTime;
+
+      let animations = this.animations.filter(
+        (animation) => !animation.finished
+      );
+
+      for (let animation of animations) {
+        let {
+          object,
+          property,
+          template,
+          start,
+          end,
+          duration,
+          timingFunction,
+          delay,
+          addTime,
+        } = animation;
+
+        let progression = timingFunction((t - delay - addTime) / duration); // 0-1 之间的数
+
+        if (t > duration + delay + addTime) {
+          progression = 1;
+          animation.finished = true;
+        }
+
+        let value = animation.valueFromProression(progression); // 根据 progression 算出当前值
+
+        object[property] = template(value);
+      }
+
+      if (animations.length) {
+        this.requestID = requestAnimationFrame(this.tick);
+      }
+    };
+  }
+
+  start() {
+    if (this.state !== "inited") {
+      return;
+    }
+
+    this.state = "playing";
+    this.startTime = Date.now();
+    this.tick();
+  }
+
+  pause() {
+    if (this.state !== "playing") {
+      return;
+    }
+
+    this.state = "paused";
+    this.pauseTime = Date.now();
+
+    if (this.requestID !== null) {
+      cancelAnimationFrame(this.requestID);
+    }
+  }
+
+  resume() {
+    if (this.state !== "paused") {
+      return;
+    }
+
+    this.state = "playing";
+    this.startTime += Date.now() - this.pauseTime;
+    this.tick();
+  }
+
+  restart() {
+    if (this.state === "playing") {
+      this.pause();
+    }
+
+    this.animations = [];
+    this.requestID = null;
+    this.state = "playing";
+    this.startTime = Date.now();
+    this.pauseTime = null;
+    this.tick();
+  }
+
+  add(animation, addTime) {
+    this.animations.push(animation);
+    animation.finished = false;
+
+    // addTime
+    // 1、添加到 Timeline 立即开始
+    // 2、从 Timeline 当前时间开始
+    if (this.state === "playing") {
+      animation.addTime =
+        addTime !== void 0 ? addTime : Date.now() - this.startTime;
+    } else {
+      animation.addTime = addTime !== void 0 ? addTime : 0;
+    }
+  }
+}
+
+export class Animation {
+  constructor(
+    object,
+    property,
+    template,
+    start,
+    end,
+    duration,
+    timingFunction,
+    delay
+  ) {
+    this.object = object;
+    this.property = property;
+    this.template = template;
+    this.start = start;
+    this.end = end;
+    this.duration = duration;
+    this.timingFunction = timingFunction;
+    this.delay = delay;
+  }
+
+  valueFromProression(progression) {
+    return this.start + progression * (this.end - this.start);
+  }
+}
---
+++ week19/19.ToyTool/generators/app/templates/main.test.js
@@ -0,0 +1,6 @@
+import "../src/main.js";
+let assert = require("assert");
+
+it("foo", () => {
+  assert.equal(8, 8);
+});
